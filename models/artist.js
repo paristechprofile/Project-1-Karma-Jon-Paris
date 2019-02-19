79a1e212ac3a4044d343e50a43c27f33089d750d
Paris Taylor <paristechprofile@gmail.com>
2019-02-19 15:07:29 -0800

    definitely messed something up with the branches... sorry

--- models/artist.js
+++ models/artist.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
+=======
+const
+    mongoose = require(`mongoose`),
+    Schema = mongoose.Schema;
+
+>>>>>>> ed506ccc2ca9dd270cc8de209d0dc375b235f83b
 const ArtistSchema = new Schema({
         name: String,
         artistUrl: String,
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 // import model files 
+<<<<<<< HEAD
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const db = require(`./models`);
+=======
 const
     express = require(`express`),
     bodyParser = require(`body-parser`),
     db = require(`./models`);
+>>>>>>> ed506ccc2ca9dd270cc8de209d0dc375b235f83b
 
 //set app to axpress library
 let app = express();
 
+<<<<<<< HEAD
+=======
 console.log("Just testing out... hello hello")
 
+>>>>>>> ed506ccc2ca9dd270cc8de209d0dc375b235f83b
 //serve static files from public folder
 app.use(express.static(`public`));
 
@@ -27,5 +36,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(process.env.PORT || 3000, function() {
    console.log(`Catchy listening at localhost3000`);
+<<<<<<< HEAD
+});
+=======
 })
 
+>>>>>>> ed506ccc2ca9dd270cc8de209d0dc375b235f83b
