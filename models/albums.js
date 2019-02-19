4b4fd5f43b0b8fbec8641dcf573594e0d77f22be
Paris Taylor <paristechprofile@gmail.com>
2019-02-19 14:59:24 -0800

    made the front-end

--- models/albums.js
+++ models/albums.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 
 const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
-=======
-const
-    mongoose = require(`mongoose`),
-    Schema = mongoose.Schema;
->>>>>>> ed506ccc2ca9dd270cc8de209d0dc375b235f83b
     
 const AlbumSchema = new Schema({
         name: String,
