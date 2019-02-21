e3d49451d3fbb7b6dc7aa2af82e71368683a66b8
Paris Taylor <paristechprofile@gmail.com>
2019-02-21 13:56:27 -0800

    updating public scripts app.js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,25 +5,22 @@ $(document).ready(() => {
 // click event
 $(`#updateProfileBtn`).addEventListener("click", updateProfile, false) //listen for click event
 function updateProfile(e) { //create a function that saves div as a variable and then looks for 
-let saveProfile = $('#updateProfileBtn');
+/* let saveProfile = $('#updateProfileBtn'); */
     e.preventDefault();
     e.stopPropagation();
 
     $.ajax({
         method: 'GET',
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=',
-        data: {
-          id: 123
-        },
+        url: 'localhost:3000/api/user/5c6f0a07942d661a3aa6a4db',
         success: function( response ) {
-          $('#temp').html('The temperature in Detroit is ' + response.temp);
+            res.name $('#temp').html('The temperature in Detroit is ' + response.temp);
         },
         error: function() {
-          alert('There was an error getting weather data.');
+            alert('There was an error saving your profile information. Please try again later.');
         },
         complete: function () {
-          $('#updateProfileBtn').prepend(`<h6>Saved!</h6>`);
+            $('#updateProfileBtn').prepend(`<h6>Saved!</h6>`);
         }
-  });
+    });
     
 };
