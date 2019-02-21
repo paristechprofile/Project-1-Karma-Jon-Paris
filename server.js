db8d2ae214f04f42694c2168706344eec2af0293
Paris Taylor <paristechprofile@gmail.com>
2019-02-20 21:01:51 -0800

    added index to public folder

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
  */
 
  app.get('/',(req,res)=>{
-    res.sendFile(__dirname + 'index.html')
+    res.sendFile(__dirname + '/views/index.html')
  })
 
 
