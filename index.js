6303387fa8932e07ad3bb953cc4df19a7d56c968
Paris Taylor <paristechprofile@gmail.com>
2019-02-20 20:45:39 -0800

    adding user profile html again

--- index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/catchy', { useNewUrlParser: true } );
-
-module.exports = {
-    Album : require('./album'),
-    Artist: require('./artist'),
-}
-
